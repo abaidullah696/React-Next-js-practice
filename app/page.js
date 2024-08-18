742d732bdd354c608289340fe358745e9c997d6b
abaidullah696 <abaidullah696@gmail.com>
2024-08-18 23:11:15 +0500

    Call api to fetch data from json placeholder

--- app/page.js
+++ app/page.js
@@ -1,10 +1,24 @@
-import React from 'react'
+"use client"
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const page = () => {
+  
+const [users, setusers] = useState([])
+const getUser = async () => {
+  const {data} = await axios("https://jsonplaceholder.typicode.com/users")
+  setusers(data)
+}
   return (
     <>
-    <h1 className='text-5xl' >This is home page</h1>
-    <a className='text-3xl' href='contact'>Contact</a>
+    <button onClick={getUser} className='bg-green-500 px-5 py-4 rounded m-10'>Get Data</button>
+    <div className='p-8 bg-slate-100 mt-5'>
+        <ul>
+          {users.map((e)=>{
+            return <li>{e.username}</li>
+          })}
+        </ul>
+    </div>
     </>
   )
 }
